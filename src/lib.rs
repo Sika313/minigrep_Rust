ca41cc6b35ebef8e6f3d847bfa4623cfafbf91c2
Mashekwa Sika <msikatema@gmail.com>
2023-05-31 11:08:07 +0200

    Used iterator arguments in main, new and run funtions.

--- src/lib.rs
+++ src/lib.rs
@@ -40,13 +40,7 @@ Trust me.";
 }
 
 pub fn search<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() { 
-        if line.contains(query){ 
-            results.push(line);
-        }
-    }
-    results
+    contents.lines().filter(|line| line.contains(query)).collect()
 }
 
 pub fn case_insensitive<'a>(query: &str, contents:&'a str) -> Vec<&'a str> {
@@ -67,13 +61,20 @@ pub struct Config {
 }
 
 impl Config {
-pub fn new(args: &[String]) -> Result<Config, &'static str> { 
-    if args.len() < 3 { 
-        return  Err("Arguments too few!");
-    }
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> { 
+    args.next();
+    
+    let query = match args.next() { 
+        Some(arg) => arg,
+        None => return Err("Didn't get a query string."),
+    };
+
+    let filename = match args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a file name."),
+    };
+
+   let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
     Ok(Config{query, filename, case_sensitive})
 }
 
